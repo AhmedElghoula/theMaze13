2ea0c60cec942935450954cffd2fa30686184147
Ahmed Elghoula <ahmed.elghoula27@gmail.com>
2024-04-21 06:49:37 +0100

    first commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+            SHAZ
+            </div>
+          </div>
+        </div>
+      </div>
+      Our website offers a diverse range of farm-fresh products, carefully cultivated and harvested to deliver unparalleled quality and taste. From vibrant fruits bursting with flavor to crisp, nutrient-rich vegetables harvested at peak ripeness, our selection reflects the bounty of nature's harvest. Additionally, we pride ourselves on providing premium cuts of meat sourced from ethically-raised livestock, ensuring both exceptional taste and sustainable farming practices. Furthermore, our assortment extends beyond produce and proteins, featuring artisanal goods such as homemade preserves, organic honey, and handcrafted cheeses, each crafted with dedication and expertise. Whether you're seeking wholesome ingredients for your culinary creations or simply craving the authentic taste of farm-fresh goodness, our website is your ultimate destination for premium agricultural delights.
+    </>
+
+
+  
+  );
+};
+export default About;
---
+++ client/src/utils/index.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const productionUrl = ' http://localhost:5000/api/v1';
+
+export const customFetch = axios.create({
+ // withCredentials: true,
+  baseURL: productionUrl,
+  //headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
+	//credentials: 'include',
+});
+
+
+export const customFetchwithCred = axios.create({
+   withCredentials: true,
+   baseURL: productionUrl,
+  
+ });
+ 
+
+export const formatPrice = (price) => {
+  const dollarsAmount = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format((price/100).toFixed(2));
+  return dollarsAmount;
+};
+
+export const generateAmountOptions = (number) => {
+  return Array.from({ length: number }, (_, index) => {
+    const amount = index + 1;
+    return (
+      <option key={amount} value={amount}>
+        {amount}
+      </option>
+    );
+  });
+};
